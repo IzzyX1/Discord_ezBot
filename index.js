ddac05d7452df1829c91d8cf610e4c9ed7bf1bdf
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-05-24 17:09:25 +0100

    Anti-Spam Revert.

--- index.js
+++ index.js
@@ -6,31 +6,22 @@ const request = require('request');
 const ping = require('minecraft-server-util');
 const usedCommandRecently = new Set();
 const AntiSpam = require('discord-anti-spam');
-let kickEmbed = new Discord.MessageEmbed()
-.setColor('#801b1b')
-.setTitle('Warning!')
-.setDescription('**{user_tag}** has been kicked for spamming.')
-.setTimestamp();
-
-let banEmbed = new Discord.MessageEmbed()
-.setColor('#801b1b')
-.setTitle('Warning!')
-.setDescription('**{user_tag}** has been banned for spamming.')
-.setTimestamp();
-
 const antiSpam = new AntiSpam({
-    warnThreshold: 3,
-    kickThreshold: 5,
-    banThreshold: 7,
-    maxInterval: 2000,
-    warnMessage: warnEmbed,
-    kickMessage: kickEmbed,
-    banMessage: banEmbed,
-    maxDuplicatesWarning: 3,
-    maxDuplicatesKick: 10,
-    maxDuplicatesBan: 12,
-    ignoreBots: true,
-    verbose: true
+    warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
+    kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+    banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+    maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+    warnMessage: '{@user}, Please stop spamming.', // Message that will be sent in chat upon warning a user.
+    kickMessage: '**{user_tag}** has been kicked for spamming.', // Message that will be sent in chat upon kicking a user.
+    banMessage: '**{user_tag}** has been banned for spamming.', // Message that will be sent in chat upon banning a user.
+    maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
+    exemptPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
+    ignoreBots: true, // Ignore bot messages.
+    verbose: true, // Extended Logs from module.
+    ignoredUsers: [], // Array of User IDs that get ignored.
+    // And many more options... See the documentation.
 });
 //-----------------------------------------------------------------------------------------------
 
