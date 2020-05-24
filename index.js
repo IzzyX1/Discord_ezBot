ed2fdc779c67e5b56be90c6e0c56e5e053e259d1
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-05-24 17:20:44 +0100

    Anti-Spam Fix v4

--- index.js
+++ index.js
@@ -8,23 +8,25 @@ const usedCommandRecently = new Set();
 const AntiSpam = require('discord-anti-spam');
 const antiSpam = new AntiSpam({
     warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
-    kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+    kickThreshold: 5, // Amount of messages sent in a row that will cause a ban.
     banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
     maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
     warnMessage: '{@user}, Please stop spamming.', // Message that will be sent in chat upon warning a user.
     kickMessage: '**{user_tag}** has been kicked for spamming.', // Message that will be sent in chat upon kicking a user.
     banMessage: '**{user_tag}** has been banned for spamming.', // Message that will be sent in chat upon banning a user.
-    maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
-    maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
-    maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesWarning: 3, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesKick: 5, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesBan: 7, // Amount of duplicate messages that trigger a warning.
     exemptPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
     ignoreBots: true, // Ignore bot messages.
     verbose: true, // Extended Logs from module.
     ignoredUsers: [], // Array of User IDs that get ignored.
     // And many more options... See the documentation.
 });
+bot.on("message", async (message) => {
+  AntiSpam.message(message
+)})
 //-----------------------------------------------------------------------------------------------
-
 bot.on('ready', () => {
     console.log('ezBot is online!')
     let activities = ["$help", `${bot.guilds.cache.size} Servers`];
