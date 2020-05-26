4a02499039a4bd2d52ac0a3c54ef7c4349de0c48
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-05-26 13:38:12 +0100

    Added Flushed

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ bot.on('message', message =>{
         case 'help':
             const embed = new Discord.MessageEmbed()
             .setTitle('Commands Help:')
-            .addField('Commands:', 'ban, kick, meme, clown, dead, rip, cri, omg, thonk, nou, poggers, clear, mc, minesweeperhelp, gifhelp, mchelp, antispamhelp')
+            .addField('Commands:', 'ban, kick, meme, clown, dead, rip, cri, omg, thonk, nou, flushed, poggers, clear, mc, minesweeperhelp, gifhelp, mchelp, antispamhelp')
             .addField('Support Server:', 'https://discord.gg/ac3KQu2')
             .setColor(0x03C4FF)
             .setThumbnail(message.author.avatarURL)
@@ -230,6 +230,9 @@ bot.on('message', message =>{
         break;
         case 'rip':
             message.react('🙏');
+        break;
+        case 'flushed':
+            message.react('😳');
         break;
 //------------------------------------------------------ COOL DOWN GIF POGGERS COMMAND ------------------------------------        
         case 'poggers':
