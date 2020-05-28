d5ded03228ceaacc1d916baef6f7b180e39620a9
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-05-28 20:56:48 +0100

    Change meme

--- index.js
+++ index.js
@@ -375,7 +375,7 @@ bot.on('message', message =>{
 });
 function image(message){
     var options = {
-        url: "https://images.google.com/?gws_rd=ssl" + "reddit memes",
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "redditmemes",
         method: "GET",
         headers: {
             "Accept": "text/html",
