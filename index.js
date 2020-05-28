580624ebd1c14ebf946394aa94d12c85bcc90e57
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-05-28 21:59:25 +0100

    Watching

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', (message) => antiSpam.message(message)); // Anti-Spam Function
 bot.on("ready", () => {
     // This event will run if the bot starts, and logs in, successfully.
     console.log(`ezBot has started, with ${bot.users.cache.size} users, in ${bot.channels.cache.size} channels of ${bot.guilds.cache.size} guilds.`); 
-    bot.user.setActivity(`In ${bot.guilds.cache.size} Servers | $help`, { type: 'STREAMING'});
+    bot.user.setActivity(`In ${bot.guilds.cache.size} Servers | $help`, { type: 'WATCHING'});
   });
   
   bot.on("guildCreate", guild => {
