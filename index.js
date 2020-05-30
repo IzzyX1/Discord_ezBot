67b72bc3e56a1b3c77226c3469a0c374a311644a
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-05-30 01:11:46 +0100

    RPS

--- index.js
+++ index.js
@@ -397,6 +397,40 @@ function image(message){
         message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
     });
 }
+client.on('message', message => {
+
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'rps') {
+        let replies = ['rock', 'paper', 'scissors'];
+        let result = Math.floor((Math.random() * replies.length));
+
+        let uReply = args[0];
+        if (!uReply) return message.channel.send(`Please play with one of these responses: \`${replies.join(', ')}\``);
+        if (!replies.includes(uReply)) return message.channel.send(`Only these responses are accepted: \`${replies.join(', ')}\``);
+
+        if (replies[result] === uReply) {
+            console.log(replies[result]);
+            return message.channel.send('It\'s a tie! We had the same choice.');
+        } else if (uReply === 'rock') {
+            console.log(replies[result]);
+            if (replies[result] === 'paper') return message.channel.send('I won!');
+            else return message.channel.send('You won!');
+        } else if (uReply === 'scissors') {
+            console.log(replies[result]);
+            if (replies[result] === 'rock') return message.channel.send('I won!');
+            else return message.channel.send('You won!');
+        } else if (uReply === 'paper') {
+            console.log(replies[result]);
+            if (replies[result] === 'scissors') return message.channel.send('I won!');
+            else return message.channel.send('You won!');
+        }
+    }
+});
 // ---------------------------------- COMMANDS ABOVE ----------------------------------------------
 bot.login(process.env.token);
 //------------------------------------- ADDITIONAL NOTES: -----------------------------------------
