2b7041bc4a2c9f1d189eb060ac9afbdcb9722a2e
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-06-01 20:10:36 +0100

    Experiment continued

--- commands/rob.js
+++ commands/rob.js
@@ -41,8 +41,9 @@ let embed = new Discord.MessageEmbed()
 .setColor("#00FF00")
 message.channel.send(embed)
 
-db.add(`money_${message.author.id}_${user.id}`, random)
-db.subtract(`money_${message.guild.id}_${user.id}`, random)
+db.subtract(`money_${user.id}`, random)
+db.add(`money_${message.author.id}`, random)
+
   
 };
 }
