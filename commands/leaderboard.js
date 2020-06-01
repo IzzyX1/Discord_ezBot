9b62783223d3c430e1832371dc26523fc9c113c4
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-06-01 20:35:10 +0100

    Hopefully this will work

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,24 +3,16 @@ const db = require('quick.db')
 
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith('e$'))return; 
-
-    let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data'})
-    let content = "";
-
-    for (let i = 0; i < money.length; i++) {
-        let user = bot.users.get(money[i].ID.split('_')[2]).user
-
-        content += `${i+1}. ${user} ~ ${money[i].data}$\n`
+  let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data'})
+  let content = "";
+  for (let i = 0; i < money.length; i++) {
+    let user = bot.users.get(money[i].ID.split('_')[2]).username
+      content += `${i+1}. ${user} ~ ${money[i].data}\n`
+  
     }
 
-    const embed = new Discord.MessageEmbed()
-    .setAuthor(`${message.guild.name} - Leaderboard:`, message.guild.iconURL)
-    .setDescription(content)
-    .setColor(0x51267)
-
-    message.channel.send(embed)
-
-
-
+  const embed = new Discord.RichEmbed()
+  .setDescription(`**${message.guild.name}'s Leaderboard**\n\n${content}`)
+  .setColor("#800080")
 
 }
