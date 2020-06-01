1542245c3af49b271977042470d97ede70259c16
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-06-01 18:28:05 +0100

    Potential Pay Fix

--- commands/pay.js
+++ commands/pay.js
@@ -1,54 +1,54 @@
-//const Discord = require("discord.js");
-//const db = require("quick.db");
-//const ms = require("parse-ms");
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
 
-//module.exports.run = async (bot, message, args) => {
-// if(!message.content.startsWith('e$'))return;  
+module.exports.run = async (bot, message, args) => {
+  if(!message.content.startsWith('e$'))return;  
 
-  //let user = message.mentions.members.first() 
+  let user = message.mentions.members.first() 
 
- // let member = db.fetch(`money_${message.guild.id}_${message.author.id}`)
+  let member = db.fetch(`money_${message.guild.id}_${message.author.id}`)
 
- // let embed1 = new Discord.RichEmbed()
-  //.setColor("#ff0000")
-  //.setDescription(`❌ Mention someone to pay`);
+  let embed1 = new Discord.MessageEmbed()
+  .setColor("#ff0000")
+  .setDescription(`❌ Mention someone to pay`);
 
-  ///if (!user) {
- //     return message.channel.send(embed1)
- // }
- // let embed2 = new Discord.RichEmbed()
- // .setColor("#ff0000")
-  //.setDescription(`❌ Specify an amount to pay`);
+  if (!user) {
+      return message.channel.send(embed1)
+  }
+  let embed2 = new Discord.MessageEmbed()
+  .setColor("#ff0000")
+  .setDescription(`❌ Specify an amount to pay`);
   
- // if (!args[1]) {
-  //    return message.channel.send(embed2)
-  //}
- // let embed3 = new Discord.RichEmbed()
-  //.setColor("#ff0000")
-  //.setDescription(`❌ You can't pay someone negative money`);
+  if (!args[1]) {
+      return message.channel.send(embed2)
+  }
+  let embed3 = new Discord.MessageEmbed()
+  .setColor("#ff0000")
+  .setDescription(`❌ You can't pay someone negative money`);
 
-  //if (message.content.includes('-')) { 
-  //    return message.channel.send(embed3)
-  //}
-  //let embed4 = new Discord.RichEmbed()
- // .setColor("#ff0000")
-  //.setDescription(`❌ You don't have that much money`);
+  if (message.content.includes('-')) { 
+      return message.channel.send(embed3)
+  }
+  let embed4 = new Discord.MessageEmbed()
+  .setColor("#ff0000")
+  .setDescription(`❌ You don't have that much money`);
 
-  //if (member < args[1]) {
-  //    return message.channel.send(embed4)
-  //}
+  if (member < args[1]) {
+      return message.channel.send(embed4)
+  }
 
- // let embed5 = new Discord.RichEmbed()
- // .setColor("#00FF00")
- // .setDescription(`✅ You have payed ${user.user.username} ${args[1]} coins`);
+  let embed5 = new Discord.MessageEmbed()
+  .setColor("#00FF00")
+  .setDescription(`✅ You have payed ${user.user.username} ${args[1]} coins`);
 
- // message.channel.send(embed5)
- // db.add(`money_${message.guild.id}_${user.id}`, args[1])
- // db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
+  message.channel.send(embed5)
+  db.add(`money_${message.guild.id}_${user.id}`, args[1])
+  db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
 
-//}
+}
 
-//module.exports.help = {
- // name:"pay",
- // aliases: [""]
-//}
+module.exports.help = {
+  name:"pay",
+  aliases: [""]
+}
