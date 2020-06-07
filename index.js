f332297523e37f831ad9f18abdb64bbce64a0df2
AtomSmasher6666 <atomsmasher6666@gmail.com>
2020-06-07 18:47:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,15 +38,15 @@ bot.on("ready", () => {
   });
 
   
-bot.on("ready", () => {
+  bot.on("ready", () => {
     var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 
 connection.connect();
-
 connection.query('CREATE TABLE IF NOT EXISTS economy(guild_id TEXT, user_id TEXT, pocket TEXT, bank TEXT, VIP INT DEFAULT 0, sneakers INT DEFAULT 0, cars INT DEFAULT 0, mansions INT DEFAULT 0, jets INT DEFAULT 0, helicopters INT DEFAULT 0, biscuits INT DEFAULT 0, tacos INT DEFAULT 0);', function(err, rows, fields) {
   if (err) throw err;
   console.log('The solution is: ', rows[0].solution);
+connection.query("INSERT INTO economy(guild_id, user_id, pocket, bank) VALUES ('guildid', 'userid', '0', '0')")
 });
 connection.end();
 })
